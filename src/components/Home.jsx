4d4c215ed9850ad4ab22f482717949767e5c6a18
Vera <kikisvv@hotmail.com>
2018-11-14 00:04:19 -0600

    Alias add, you can create alias for your routes without any problem

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Home.scss'
+import {numbers} from '@App/numbers'
 
-const Home = () => <h1 className={styles.Home}>Home ._.)/</h1>;
+const Home = () => (
+  <div>
+    <h1 className={styles.Home}>Home ._.)/</h1>
+    {console.log(numbers)}
+  </div>
+)
 
 export default Home;
